f88d80b66c11d824c1fc48329b7a446413cc8133
Sokisati <yagizalpersoy@gmail.com>
2023-10-23 09:24:45 +0300

    rastgele değerler

--- EPL_Tx.py
+++ EPL_Tx.py
@@ -2,6 +2,7 @@
 import socket
 import time
 import json
+import random
 
 
 class GonderilecekVeriler:
@@ -24,26 +25,31 @@ class GonderilecekVeriler:
         
 
 #buraya baglanilacak pc'nin ip'si girilecek (cmd->ipconfig->ip4)
-pc_ip = '10.45.69.108' 
+pc_ip = '10.45.65.229' 
 
 #bu port degistirilebilir ama kalsin
 port = 12345
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 veriPaketNo = 0;
 
+inisHiziV = 5;
+basincV = 101.3;
+
 try:
     sock.connect((pc_ip, port))
     print("Hedef pc'ye baglanildi")
     while True:
+        
       
         data = GonderilecekVeriler(
+            
             takimNo=31,
             veriPaketNo=veriPaketNo,
             gondermeSaatiVeTarih="2023-10-20 12:00:00",
-            basinc=1013.25,
-            yukseklik=420,
-            inisHizi=5.0,
-            sicaklik=25.0,
+            basinc=basincV,
+            yukseklik=round(random.uniform(560,780),2),
+            inisHizi=inisHiziV,
+            sicaklik=round(random.uniform(2,10),2),
             pilGerilimi=12.0,
             gpsLat=35.123456,
             gpsLong=139.654321,
@@ -55,6 +61,12 @@ try:
            
         )
         veriPaketNo += 1
+        if inisHiziV <30:
+         inisHiziV = inisHiziV + 1
+         
+         if basincV >97.7:
+          basincV = basincV - round(random.uniform(0.2,0.7),2)
+        
 
         data_json = json.dumps(data.__dict__)
 
