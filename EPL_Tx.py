70e484a90189254f91e921c700d2415bf3d78781
Sokisati <yagizalpersoy@gmail.com>
2023-12-21 19:50:21 +0300

    renamed variables

--- EPL_Tx.py
+++ EPL_Tx.py
@@ -1,91 +1,78 @@
-# -- coding: utf-8 --
 import socket
 import time
 import json
-import random
-import numpy as np
 
-def encryption_function(key_vector, value_to_enc, data_packet_id):
-    key_index = data_packet_id % len(key_vector)
-    enc_value = value_to_enc * key_vector[key_index] - key_vector[key_index]
-    return enc_value
 
-key_vector = [16,81,33,32,5,15,13,71,43,8,31,72,4,38,71,19]
-
-class GonderilecekVeriler:
-    def __init__(self, takimNo, veriPaketNo, gondermeSaatiVeTarih, basinc,yukseklik, inisHizi, sicaklik, pilGerilimi, gpsLat, gpsLong, gpsAlt, pitch, roll, yaw, donusHizi):
-        self.takimNo = takimNo
-        self.veriPaketNo = veriPaketNo
-        self.gondermeSaatiVeTarih = gondermeSaatiVeTarih
-        self.yukseklik = yukseklik
-        self.basinc = basinc
-        self.inisHizi = inisHizi
-        self.sicaklik = sicaklik
-        self.pilGerilimi = pilGerilimi
+class dataPack:
+    def __init__(self, teamNumber, dataPacketNumber,dateAndTime, pressure,altitude, descentSpeed, temperature, batteryVoltage, gpsLat, gpsLong, gpsAlt, pitch, roll, yaw):
+        self.teamNumber = teamNumber
+        self.dataPacketNumber= dataPacketNumber
+        self.gondermeSaatiVeTarih =dateAndTime
+        self.altitude = altitude
+        self.pressure = pressure
+        self.descentSpeed = descentSpeed
+        self.temperature = temperature
+        self.batteryVoltage = batteryVoltage
         self.gpsLat = gpsLat
         self.gpsLong = gpsLong
         self.gpsAlt = gpsAlt
         self.pitch = pitch
         self.roll = roll
         self.yaw = yaw
-        self.donusHizi = donusHizi
-        
+       
 
-#buraya baglanilacak pc'nin ip'si girilecek (cmd->ipconfig->ip4)
-
-pc_ip =pc_ip = socket.gethostbyname(socket.gethostname())
-
-#bu port degistirilebilir ama kalsin
+pc_ip = '127.0.0.1'  
 port = 12345
+
+dataPacketNumber = 0
+pressure = 10
+altitude = 8
+temperature = 10
+dateAndTime = 'yes'
+teamNo = 1488
+descentSpeed = 15
+gpsLat = 10
+gpsLat = 10
+gpsLong = 10
+gpsAlt = 20
+pitch = 2
+yaw = 3
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-veriPaketNo = 0;
-
-basinc = 5
-yukseklik = 8
-inisHizi = 16
-sicaklik = 2
-pilGerilimi = 12
-gpsLat=35.123456
-gpsLong=139.654321
-gpsAlt=100.0
-pitch=0.0
-roll=0.0
-yaw=0.0
-donusHizi=10.0
-
 
 try:
     sock.connect((pc_ip, port))
-    print("Hedef pc'ye baglanildi")
-    while True:
-        
-      
-        data = GonderilecekVeriler(
-            
-            takimNo=31,
-            veriPaketNo=veriPaketNo,
-            gondermeSaatiVeTarih="2023-10-20 12:00:00",
-            basinc=encryption_function(key_vector,basinc,veriPaketNo),
-            yukseklik=encryption_function(key_vector,yukseklik,veriPaketNo),
-            inisHizi=encryption_function(key_vector,inisHizi,veriPaketNo),
-            sicaklik=encryption_function(key_vector,sicaklik,veriPaketNo),
-            pilGerilimi=encryption_function(key_vector,pilGerilimi,veriPaketNo),
-            gpsLat=encryption_function(key_vector,gpsLat,veriPaketNo),
-            gpsLong=encryption_function(key_vector,gpsLong,veriPaketNo),
-            gpsAlt=encryption_function(key_vector,gpsAlt,veriPaketNo),
-            pitch=0.0,
-            roll=0.0,
-            yaw=0.0,
-            donusHizi=10.0,
-           
-        )
-        veriPaketNo += 1
-       
-        
-        data_json = json.dumps(data.__dict__)
+    print("Baglanti kuruldu")
 
+    while True:
+        data = dataPack(
+            teamNumber=10,
+            dataPacketNumber=dataPacketNumber,
+            pressure=pressure,
+            altitude=altitude,
+            temperature=temperature,
+            yaw = 3,
+            pitch = 2,
+            gpsAlt = 20,
+            gpsLong = 10,
+            gpsLat = 10,
+            descentSpeed = 15,
+            batteryVoltage=20,
+            roll =10,
+            dateAndTime = 'yes',
+            
+        )
+        dataPacketNumber += 1
+        if temperature>2:
+            temperature = temperature -1
+           
+        elif temperature==2:
+            temperature = 10
+
+        data_json = json.dumps(data.__dict__)
         sock.send(data_json.encode())
 
-        time.sleep(1)
+        time.sleep(1)  
+
 except KeyboardInterrupt:
-    print("Baglanti kesildi.")
+    print("Kesildi")
